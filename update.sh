3c837567319be68d6b73c698d463a5e393f19fc5
batterX <33147905+batterX@users.noreply.github.com>
2017-11-13 10:38:07 +0100

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 # updater.sh
 
-sudo cp livesmart/html /var/www -r
+sudo cp /home/pi/livesmart/html /var/www -r
 sudo chmod 777 /var/www -R
 
 sudo rm /home/pi/BatterX
-sudo cp livesmart/BatterX /home/pi
+sudo cp /home/pi/livesmart/BatterX /home/pi
 sudo chmod 777 /home/pi/BatterX
 
-sudo cp livesmart/launcher.sh /home/pi
+sudo cp /home/pi/livesmart/launcher.sh /home/pi
 sudo chmod 777 /home/pi/launcher.sh
 
-sudo cp livesmart/updater.sh /home/pi
+sudo cp /home/pi/livesmart/updater.sh /home/pi
 sudo chmod 777 /home/pi/updater.sh
 
-sudo rm livesmart -r
+sudo rm /home/pi/livesmart -r
+
+sudo rm /home/pi/update.sh
 
 sudo reboot
--- updater.sh
+++ updater.sh
@@ -3,7 +3,7 @@
 
 git clone https://github.com/batterx/livesmart.git
 
-sudo cp livesmart/update.sh /home/pi
+sudo cp /home/pi/livesmart/update.sh /home/pi
 sudo chmod 777 /home/pi/update.sh
 
 sudo sh /home/pi/update.sh
