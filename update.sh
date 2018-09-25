62b4a0f0946adc88a95de00711e7660004767794
batterX <ivan@visionups.com>
2018-09-25 13:13:31 +0200

    v18.9.1

--- update.sh
+++ update.sh
@@ -12,6 +12,10 @@ sudo rm /home/pi/CloudStream
 sudo cp /home/pi/livesmart/CloudStream /home/pi
 sudo chmod 777 /home/pi/CloudStream
 
+sudo rm /home/pi/GetApikey
+sudo cp /home/pi/livesmart/GetApikey /home/pi
+sudo chmod 777 /home/pi/GetApikey
+
 sudo cp /home/pi/livesmart/launcher.sh /home/pi
 sudo chmod 777 /home/pi/launcher.sh
 
